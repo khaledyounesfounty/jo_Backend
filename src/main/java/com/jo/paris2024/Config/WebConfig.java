5bbd8ab2e064c957e97019939ad7583f0fa82ee5
khaledyounesfounty <Khaledyounesfounty@gmail.com>
2024-05-14 00:31:19 +0200

    adding docker file for the deployment

--- src/main/java/com/jo/paris2024/Config/WebConfig.java
+++ src/main/java/com/jo/paris2024/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*") // Use patterns to match origins
+                .allowedOriginPatterns("https://*:*") // Use patterns to match origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
