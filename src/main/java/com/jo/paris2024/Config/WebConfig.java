c5d48edd6ee57c615d74fe0cbf9392bd366917c4
khaledyounesfounty <Khaledyounesfounty@gmail.com>
2024-05-18 23:26:39 +0200

    version deployée 18-05-24

--- src/main/java/com/jo/paris2024/Config/WebConfig.java
+++ src/main/java/com/jo/paris2024/Config/WebConfig.java
@@ -1,6 +1,7 @@
+
 package com.jo.paris2024.Config;
 
-
+/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +12,32 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*:*") // Use patterns to match origins
+                .allowedOriginPatterns("http://finalexamyounesfront.s3-website.eu-west-3.amazonaws.com") // Use patterns to match origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
+}
+*/
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://finalexamyounesfront.s3-website.eu-west-3.amazonaws.com");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
